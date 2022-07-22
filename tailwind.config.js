406cb9da759b99d71ecb363d5ba7b54fa3808397
abhay57 <abhaykaushik57@gmail.com>
2022-07-22 15:22:29 +0530

    updated headlines section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clg-blue': '#0369a1',
+      },
+    },
   },
   plugins: [],
+}
+
+module.exports = {
+  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ]
 }
